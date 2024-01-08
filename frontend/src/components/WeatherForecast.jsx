e0103138024c1cdd1649420c3f0a503e9cd57936
Anurag Guleria <guleriaanurag84@gmail.com>
2024-01-09 00:47:46 +0530

    minor change in weather forecast component

--- frontend/src/components/WeatherForecast.jsx
+++ frontend/src/components/WeatherForecast.jsx
@@ -20,10 +20,10 @@ export default function WeatherForecast({coords,mode}){
         <>  
             <HorizontalLine />
             <div className="w-[100%] h-[55%] rounded-lg mt-[25px] max-md:ml-3">
-                <h2 className="max-md:text-center">Weather Forecast</h2>
+                {forecastData.length!==0 && <h2 className="max-md:text-center">Weather Forecast</h2>}
                 <div className="grid grid-cols-5 mt-10 max-md:grid-cols-1">
                     {forecastData.length!==0 && forecastData.map((item,index)=>(
-                        <div className="bg-stone-200 mx-auto w-[80%] rounded-lg flex flex-col justify-center items-center max-md:mt-4" key={index}>
+                        <div className="bg-stone-200 bg-opacity-45 mx-auto w-[80%] rounded-lg flex flex-col justify-center items-center max-md:mt-4" key={index}>
                             <p>{Intl.DateTimeFormat('en-US',{weekday:'long'}).format(new Date(item[0]))}</p>
                             <img src={`https://openweathermap.org/img/wn/${item[4]}@2x.png`} alt={item[3]} />
                             <p>{item[3]}</p>
