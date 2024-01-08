004a68dd00b5fdc56199a27b9b96aba93ba3c939
Anurag Guleria <guleriaanurag84@gmail.com>
2024-01-09 02:38:19 +0530

    some changes made to responsive design

--- frontend/src/components/WeatherForecast.jsx
+++ frontend/src/components/WeatherForecast.jsx
@@ -23,7 +23,7 @@ export default function WeatherForecast({coords,mode}){
                 {forecastData.length!==0 && <h2 className="text-md pl-4 text-stone-800 max-md:text-center">Weather Forecast</h2>}
                 <div className="grid grid-cols-5 mt-10 max-md:grid-cols-1">
                     {forecastData.length!==0 && forecastData.map((item,index)=>(
-                        <div className="bg-stone-200 bg-opacity-45 mx-auto w-[80%] rounded-lg flex flex-col justify-center items-center max-md:mt-4" key={index}>
+                        <div className="bg-stone-200 bg-opacity-45 ml-20 mx-auto w-[80%] rounded-lg flex flex-col justify-center items-center max-md:mt-4 max-md:ml-7" key={index}>
                             <p>{Intl.DateTimeFormat('en-US',{weekday:'short'}).format(new Date(item[0]))}</p>
                             <img src={`https://openweathermap.org/img/wn/${item[4]}@2x.png`} alt={item[3]} />
                             <p>{item[3]}</p>
