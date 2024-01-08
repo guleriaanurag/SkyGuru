c387d85e4f2f2669cfaa1382c3bbd9be13e15733
Anurag Guleria <guleriaanurag84@gmail.com>
2024-01-09 02:13:13 +0530

    removing speed insights from app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,10 @@
 import Main from "./components/Main";
-import {SpeedInsights} from '@vercel/speed-insights'
 import './App.css';
 import 'react-tooltip/dist/react-tooltip.css';
 
 function App() {
   return (
-    <>
       <Main />
-      <SpeedInsights/>
-    </>
   )
 }
 
